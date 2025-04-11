f4642c300f7e9d1f7192383eb756821076c55255
Axel <axel19911@gmail.com>
2025-04-11 16:45:38 +0000

    Implement Slim framework for routing and middleware in index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,29 @@
 <?php
 
-echo "Hello, World! This is a test of the PHP server.\n";
+require __DIR__ . '/../vendor/autoload.php';
+
+use Slim\Factory\AppFactory;
+use Slim\Middleware\MethodOverrideMiddleware;
+use DI\Container;
+
+session_start();
+
+$container = new Container();
+$container->set('renderer', function () {
+    return new \Slim\Views\PhpRenderer(__DIR__ . '/../templates');
+});
+$container->set('flash', function () {
+    return new \Slim\Flash\Messages();
+});
+
+$app = AppFactory::createFromContainer($container);
+$app->addErrorMiddleware(true, true, true);
+$app->add(MethodOverrideMiddleware::class);
+$router = $app->getRouteCollector()->getRouteParser();
+
+$app->get('/', function ($req, $res) {
+    $res->getBody()->write("Hello, World! This is a test of the PHP server.");
+    return $res;
+})->setName('root');
+
+$app->run();
